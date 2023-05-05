73e019cab86927169e1160ef15cf98ea4b03fecd
Sodolsky <mateuszsodolskiofficial@gmail.com>
2023-05-05 22:25:48 +0200

    Added basic register form

--- src/app/register/RegisterForm.tsx
+++ src/app/register/RegisterForm.tsx
@@ -1,22 +1,29 @@
 "use client";
 import { FormEvent, useState } from "react";
-import { loginFormInteface } from "../../../utils/interfaces";
-const baseLoginFormData: loginFormInteface = {
+import { registerInterface } from "../../../utils/interfaces";
+const baseLoginFormData: registerInterface = {
   login: "",
+  confirmLogin: "",
   password: "",
+  role: "User",
 };
-export const LoginForm = () => {
+export const RegisterForm = () => {
   const [formData, setformData] =
-    useState<loginFormInteface>(baseLoginFormData);
+    useState<registerInterface>(baseLoginFormData);
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.currentTarget;
+
     setformData((prev) => {
       return { ...prev, [name]: value };
     });
   };
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (formData.login.length === 0 || formData.password.length === 0)
+    if (
+      formData.login.length === 0 ||
+      formData.password.length === 0 ||
+      formData.confirmLogin.length === 0
+    )
       return alert("Fill out your form");
     if (formData.password.length < 8)
       return alert("Provide us with a password of length longer than 8");
@@ -28,13 +35,22 @@ export const LoginForm = () => {
       className="p-6 bg-white rounded-lg border-black shadow-xl flex flex-col justify-center gap-2 items-center "
       onSubmit={handleSubmit}
     >
-      <span className="text-2xl font-bold">Login</span>
+      <span className="text-2xl font-bold">Register</span>
       <div className="form-control">
         <label className="label text-sm">Username: </label>
         <input
           type="text"
           onChange={(e) => handleChange(e)}
           name="login"
+          className="input input-bordered"
+        />
+      </div>
+      <div className="form-control">
+        <label className="label text-sm">Confirm Username: </label>
+        <input
+          type="text"
+          onChange={(e) => handleChange(e)}
+          name="confirmLogin"
           className="input input-bordered"
         />
       </div>
@@ -47,7 +63,15 @@ export const LoginForm = () => {
           className="input input-bordered flex justify-center items-center"
         />
       </div>
-      <button className="btn btn-primary">Sign In</button>
+      <div className="flex flex-col justify-center items-center border-2 border-dashed rounded-xl p-1">
+        <label className="label text-sm">Role</label>
+        <select className="select">
+          <option value="User">User</option>
+          <option value="Restaurant">Restaurant</option>
+          <option value="Delivery">Delivery</option>
+        </select>
+      </div>
+      <button className="btn btn-primary">Sign Up</button>
     </form>
   );
 };
